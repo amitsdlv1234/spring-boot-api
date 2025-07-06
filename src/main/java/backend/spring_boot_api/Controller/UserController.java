1fb71dc096e60511eede5064fc484781e2851e45
Amit Kumar <115470270+amitsdlv1234@users.noreply.github.com>
2025-07-06 18:25:34 +0530

    Update UserController.java

--- src/main/java/backend/spring_boot_api/Controller/UserController.java
+++ src/main/java/backend/spring_boot_api/Controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
